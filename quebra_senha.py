3bf7d77fc7d7f34909d38cd640287fde0aec7858
Fernando Sousa <77690838+fernand-ux@users.noreply.github.com>
2022-07-14 22:37:28 -0300

    Add files via upload

---
+++ quebra_senha.py
@@ -0,0 +1,142 @@
+
+
+A=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0']
+B=["","","","","","",""]
+senha=input()
+
+
+n=0   
+while n < 35:
+     
+    e=0
+    f=0
+    g=0
+    h=0
+    i=0
+    j=0
+    B[0]=A[n]
+    print(B)
+    if B[0] == senha[0] and B[1]==senha[1] and B[2]==senha[2] and B[3]==senha[3] and B[4]==senha[4] and B[5]==senha[5] and B[6]==senha[6]:
+     print('entramos')
+     n=35
+     
+    else:
+      n +=  1  
+     
+    while e < 35:
+        f=0
+        g=0
+        h=0
+        i=0
+        j=0
+        B[1]=A[e]
+        print(B)
+        if B[0] == senha[0] and B[1]==senha[1] and B[2]==senha[2] and B[3]==senha[3] and B[4]==senha[4] and B[5]==senha[5] and B[6]==senha[6]:
+         print('entramos')
+         e=35
+         n=35
+         
+        else:
+         e +=  1  
+         
+        while f < 35:
+          g=0
+          h=0
+          i=0
+          j=0
+          B[2]=A[f]
+          print(B)
+          if B[0] == senha[0] and B[1]==senha[1] and B[2]==senha[2] and B[3]==senha[3] and B[4]==senha[4] and B[5]==senha[5] and B[6]==senha[6]:
+           print('entramos')
+           f=35
+           e=35
+           n=35
+           
+          else:
+           f +=  1
+           
+          while g < 35:
+              h=0
+              i=0
+              j=0
+              B[3]=A[g]
+              print(B)
+              if B[0] == senha[0] and B[1]==senha[1] and B[2]==senha[2] and B[3]==senha[3] and B[4]==senha[4] and B[5]==senha[5] and B[6]==senha[6]:
+               print('entramos')
+               g=35
+               f=35
+               e=35
+               n=35
+               
+              else:
+               g +=  1
+               
+              while h < 35:
+                   i=0
+                   j=0
+                   B[4]=A[h]
+                   print(B)
+                   if B[0] == senha[0] and B[1]==senha[1] and B[2]==senha[2] and B[3]==senha[3] and B[4]==senha[4] and B[5]==senha[5] and B[6]==senha[6]:
+                    print('entramos')
+                    h=35
+                    g=35
+                    f=35
+                    e=35
+                    n=35
+                    
+                   else:
+                    h +=  1  
+                    
+                   while i < 35:
+                         j=0
+                         B[5]=A[i]
+                         print(B)
+                         if B[0] == senha[0] and B[1]==senha[1] and B[2]==senha[2] and B[3]==senha[3] and B[4]==senha[4] and B[5]==senha[5] and B[6]==senha[6]:
+                          print('entramos')
+                          i=35
+                          h=35
+                          g=35
+                          f=35
+                          e=35
+                          n=35
+                          
+                         else:
+                          i +=  1  
+                          
+                         while j < 35:
+                              B[6]=A[j]
+                              print(B)
+                              if B[0] == senha[0] and B[1]==senha[1] and B[2]==senha[2] and B[3]==senha[3] and B[4]==senha[4] and B[5]==senha[5] and B[6]==senha[6]:
+                               print('entramos')
+                               j=35
+                               i=35
+                               h=35
+                               g=35
+                               f=35
+                               e=35
+                               n=35
+                               
+                              else:
+                               j +=  1
+                                
+        
+
+
+
+
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+              
+    
